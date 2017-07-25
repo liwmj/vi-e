8a7f962f625b861ae5cd81e8adf7247e63205fbd
Wim Li <liwangmj@gmail.com>
2017-07-25 16:42:36 +0800

    增加vi配置

--- vimrc/install.sh
+++ vimrc/install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+cd $(dirname $0)
+
+mkdir -p ~/.vim
+cp -rfp _vimrc ~/.vimrc
+cp -rfp vimfiles/* ~/.vim/
+vim +PluginInstall +qall
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.sh --clang-completer
 
 exit 0
 
